f937a9182194436bb5510a8cb22acf872f9af291
= <peik.kalliokoski@helsinki.fi>
2023-02-15 14:42:47 +0200

    Update test data

--- test-data.sql
+++ test-data.sql
@@ -1,4 +1,11 @@
+INSERT INTO Types (name) VALUES
+	('Fire'), ('Flying'), ('Water');
 
-INSERT INTO Monsters (name, type1, type2) VALUES
-	('Charizard', 'Fire', 'Flying'),
-	('Growlithe', 'Fire', '');
+INSERT INTO Pokemon (
+	name, type1, type2,
+	health, attack, defence, spc_att, spc_def, speed
+) VALUES (
+	'Charizard',
+	(SELECT id FROM Types WHERE name='Fire'),
+	(SELECT id FROM Types WHERE name='Flying'),
+	78, 84, 78, 109, 85, 100);
